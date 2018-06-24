a68e01e556baa3b0feee43ab4eec51da41022616
Steffan Kirk <superkirky@gmail.com>
2018-06-25 00:54:30 +0200

    added std to matlab code, LOL

--- code/nineteenthjunewindtunneldata.m
+++ code/nineteenthjunewindtunneldata.m
@@ -10,6 +10,7 @@ relativePortLocation = pressureportFrontDistance./chordLength;
 
 %AOA, Straingauge Force, wind speed matrix:
 AOA_force_speed = [];
+AOA_force_speed_std = [];
 
 
 %V=10m/s
@@ -17,108 +18,126 @@ data = importdata('day1test/CarWing_V10_A00.lvm');
 meanPressureReadingsV10 = mean(data(:,1:15));
 standardDeviationV10 = std(data(:,1:15));
 AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
 %V=20m/s
 data = importdata('day1test/CarWing_V20_A00.lvm');
 meanPressureReadingsV20 = mean(data(:,1:15));
 standardDeviationV20 = std(data(:,1:15));
 AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = -4
     data = importdata('day2test/CarWing_V20_A04m.lvm');
     meanPressureReadingsV20_A04m = mean(data(:,1:15));
     standardDeviationV20_A04m = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = -7
     data = importdata('day2test/CarWing_V20_A07m.lvm');
     meanPressureReadingsV20_A07m = mean(data(:,1:15));
     standardDeviationV20_A07m = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = -10
     data = importdata('day2test/CarWing_V20_A10m.lvm');
     meanPressureReadingsV20_A10m = mean(data(:,1:15));
     standardDeviationV20_A10m = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = +4
     data = importdata('day2test/CarWing_V20_A04p.lvm');
     meanPressureReadingsV20_A04p = mean(data(:,1:15));
     standardDeviationV20_A04p = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = +7
     data = importdata('day2test/CarWing_V20_A07p.lvm');
     meanPressureReadingsV20_A07p = mean(data(:,1:15));
     standardDeviationV20_A07p = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = +10
     data = importdata('day2test/CarWing_V20_A10p.lvm');
     meanPressureReadingsV20_A10p = mean(data(:,1:15));
     standardDeviationV20_A10p = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
 %V=30m/s
 data = importdata('day1test/CarWing_V30_A00.lvm');
 meanPressureReadingsV30 = mean(data(:,1:15));
 standardDeviationV30 = std(data(:,1:15));
 AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
 %V=40m/s
 data = importdata('day1test/CarWing_V40_A00.lvm');
 meanPressureReadingsV40 = mean(data(:,1:15));
 standardDeviationV40 = std(data(:,1:15));
 AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = -4
     data = importdata('day2test/CarWing_V40_A04m.lvm');
     meanPressureReadingsV40_A04m = mean(data(:,1:15));
     standardDeviationV40_A04m = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = -7
     data = importdata('day2test/CarWing_V40_A07m.lvm');
     meanPressureReadingsV40_A07m = mean(data(:,1:15));
     standardDeviationV40_A07m = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = -10
     data = importdata('day2test/CarWing_V40_A10m.lvm');
     meanPressureReadingsV40_A10m = mean(data(:,1:15));
     standardDeviationV40_A10m = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = +4
     data = importdata('day2test/CarWing_V40_A04p.lvm');
     meanPressureReadingsV40_A04p = mean(data(:,1:15));
     standardDeviationV40_A04p = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = +7
     data = importdata('day2test/CarWing_V40_A07p.lvm');
     meanPressureReadingsV40_A07p = mean(data(:,1:15));
     standardDeviationV40_A07p = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
     %AOA = +10
     data = importdata('day2test/CarWing_V40_A10p.lvm');
     meanPressureReadingsV40_A10p = mean(data(:,1:15));
     standardDeviationV40_A10p = std(data(:,1:15));
     AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+    AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
 %V=50m/s
 data = importdata('day2test/CarWing_V50_A00p.lvm');
 meanPressureReadingsV50 = mean(data(:,1:15));
 standardDeviationV50 = std(data(:,1:15));
 AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
 %V=60m/s
 data = importdata('day2test/CarWing_V60_A00p.lvm');
 meanPressureReadingsV60 = mean(data(:,1:15));
 standardDeviationV60 = std(data(:,1:15));
 AOA_force_speed = [AOA_force_speed; mean(data(:,[65:66, 69]))];
+AOA_force_speed_std = [AOA_force_speed_std; std(data(:,[65:66, 69]))];
 
 %Cleaning AOA force speed data
 AOA_force_speed = [round(AOA_force_speed(:,1)) round(AOA_force_speed(:,2),1) round(AOA_force_speed(:,3),2)];
@@ -234,6 +253,15 @@ AOA20 = AOA_force_speed(2:8,1);
 DF20  = AOA_force_speed(2:8,2);
 AOA40 = AOA_force_speed(10:16,1)
 DF40  = AOA_force_speed(10:16,2)
+V20_mean   = mean(AOA_force_speed(2:8,3));
+V40_mean   = mean(AOA_force_speed(10:16,3));
+
+DF20_std = std(AOA_force_speed_std(2:8,2)); % std of downforce @ v=20 m/s
+DF40_std = std(AOA_force_speed_std(10:16,2));
+V20_std  = std(AOA_force_speed_std(2:8,3));
+V40_std  = std(AOA_force_speed_std(10:16,3));
+
+
 C_L   = DF20
 b = 0.25; %250mm
 c = 0.155; %150 mm
